63dba59cf234b7035c9f28657bac63bab5061d94
Ytherambugua <mbugualinnete@gmail.com>
2023-09-18 00:31:57 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,25 +1,17 @@
 #include "main.h"
 
 /**
- * _putchar - writes a character to stdout.
- * @c: The character to print.
- * Return: On success, 1 is returned. On error, -1 is returned.
- */
-int _putchar(char c);
-
-/**
  * _puts - a function that prints a string.
  * @str: string input
  * Return: string
  */
 void _puts(char *str)
 {
-        int i;
+	int i;
 
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                _putchar(str[i]);
-        }
-        _putchar('\n');
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
-
